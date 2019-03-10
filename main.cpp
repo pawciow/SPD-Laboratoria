72d4fa1da432d0d8663e0023506a17cac6f326b5
Paweł Więcław <pawciow123@gmail.com>
2019-03-10 17:27:22 +0100

    Walczę ze środowiskiem XD

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <list>
+#include <initializer_list>
+#include <map>
+
+using timesForJobs = std::vector<std::pair<const int, const int>>; /*Nazwa niefortunna, trzeba zmieniæ XD
+																	Generalnie chodzi o pary:(Numer zadania, Czas wykonania)*/
+class Machine
+{
+public:
+	Machine() = default;
+	void LoadTimes(timesForJobs times);
+private:
+	timesForJobs _timesForJobs;
+
+};
+
+class JohnsonsTwoMachineAlgorithm
+{
+public:
+	JohnsonsTwoMachineAlgorithm() = default;
+	enum class MachineNumber { First,Second};
+	void LoadTimesForMachines(MachineNumber whichMachine, timesForJobs times);
+private:
+	Machine _firstMachine;
+	Machine _secondMachine;
+};
+
+void Machine::LoadTimes(timesForJobs times)
+{
+	for (auto & e : times)
+	{
+		_timesForJobs.push_back(e);
+	}
+}
+
+void JohnsonsTwoMachineAlgorithm::LoadTimesForMachines(JohnsonsTwoMachineAlgorithm::MachineNumber whichMachine, timesForJobs times)
+{
+	if (whichMachine == JohnsonsTwoMachineAlgorithm::MachineNumber::First)
+	{
+		_firstMachine.LoadTimes(times);
+	}
+	else if (whichMachine == JohnsonsTwoMachineAlgorithm::MachineNumber::Second)
+	{
+		_secondMachine.LoadTimes(times);
+	}
+	else
+	{
+		std::cerr << "Wrong machine number!" << std::endl;
+		exit(1);
+	}
+}
+
+int main()
+{
+	JohnsonsTwoMachineAlgorithm exampleAlgorithm;
+	timesForJobs exampleTimes{ {1,3},{2,4},{3,5} }; 
+	exampleAlgorithm.LoadTimesForMachines(JohnsonsTwoMachineAlgorithm::MachineNumber::First, exampleTimes);
+	// MÊCZÊ SIÊ Z VS ¯EBY WGL TO SKOMPILOWAÆ WSZYSTKO XD
+	return 0;
+}
