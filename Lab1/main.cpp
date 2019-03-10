d1b54ecf77049d883d37b974b09aa94e1fff87d4
Paweł Więcław <pawciow123@gmail.com>
2019-03-10 20:58:37 +0100

    Po spotkaniu w kawiarni. TODO: ALGORITHM

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -4,6 +4,7 @@
 #include <list>
 #include <initializer_list>
 #include <map>
+#include <algorithm>
 
 using timesForJobs = std::vector<std::pair<const int, const int>>; /*Nazwa niefortunna, trzeba zmieniæ XD
 																	Generalnie chodzi o pary:(Numer zadania, Czas wykonania)*/
@@ -12,9 +13,9 @@ class Machine
 public:
 	Machine() = default;
 	void LoadTimes(timesForJobs times);
-private:
 	timesForJobs _timesForJobs;
-
+	
+	std::pair<const int, const int> findMinTime();
 };
 
 class JohnsonsTwoMachineAlgorithm
@@ -23,6 +24,7 @@ public:
 	JohnsonsTwoMachineAlgorithm() = default;
 	enum class MachineNumber {First,Second};
 	void LoadTimesForMachines(MachineNumber whichMachine, timesForJobs times);
+	void DO();
 private:
 	Machine _firstMachine;
 	Machine _secondMachine;
@@ -53,10 +55,27 @@ void JohnsonsTwoMachineAlgorithm::LoadTimesForMachines(JohnsonsTwoMachineAlgorit
 	}
 }
 
+std::pair<const int, const int> Machine::findMinTime()
+{
+	/*TODO*/
+}
+/*http://kkapd.f11.com.pl/zsw/Algorytm_Johnsona/przyklad_Johnson.htm Przyk³ad który bêdziemy odtwarzaæ*/
+void JohnsonsTwoMachineAlgorithm::DO()
+{
+	std::list<int> jobsToConsiderate{ 1,2,3,4,5 };
+	/*TODO*/
+	//while (jobsToConsiderate.size() != 0)
+	{
+		//int lowestFromFirst = std::min_element(_firstMachine._timesForJobs.begin(), _firstMachine._timesForJobs.end(),[]());
+	}
+}
+
 int main()
 {
 	JohnsonsTwoMachineAlgorithm exampleAlgorithm;
-	timesForJobs exampleTimes{ {1,3},{2,4},{3,5} }; 
+	timesForJobs exampleTimes{ {1,6},{2,0},{3,5}, {4,8}, {5,2} };
 	exampleAlgorithm.LoadTimesForMachines(JohnsonsTwoMachineAlgorithm::MachineNumber::First, exampleTimes);
+	timesForJobs exampleTimes2{ {1,5}, {2,1}, {3,4}, {4,8}, {5,3} };
+	exampleAlgorithm.LoadTimesForMachines(JohnsonsTwoMachineAlgorithm::MachineNumber::Second, exampleTimes2);
 	return 0;
 }
