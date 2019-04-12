2f5ab1e4fb20afbc22826cc3a6789dcbb11c74ca
Turandor <48029803+Turandor@users.noreply.github.com>
2019-04-12 15:34:30 +0200

    wersja do testów

--- Lab1/SA_algorithm.h
+++ Lab1/SA_algorithm.h
@@ -1,30 +1,30 @@
-#include "NEH.H"
+#include "NEH.h"
 #include <random>
 #include <cstdlib>
 class SA : public NEH
 {
-public:
-	SA() = delete;
-	SA(int startingTemperature, double iterationStep)
-		:_temperature(startingTemperature), _step(iterationStep)
-	{
-		std::cout << "Utworzono obiekt SA z parametrami: " << _temperature << " , " << _step;
-	}
-	void getStartingState();
-	void setExampleFromFile(std::string fileName);
-	void StartComputing();
-	int _endTime;
+	public:
+		SA() = delete;
+		SA(int startingTemperature, double iterationStep)
+			:_temperature(startingTemperature), _step(iterationStep)
+		{
+			std::cout << "Utworzono obiekt SA z parametrami: " << _temperature << " , " << _step;
+		}
+		void getStartingState();
+		void setExampleFromFile(std::string fileName);
+		void StartComputing();
+		int _endTime;
 
-protected:
-	int _temperature; 
-	double _step;
-	std::vector<std::pair<int, int>> newInstance;
+	protected:
+		int _temperature;
+		double _step;
+		std::vector<std::pair<int, int>> newInstance;
 
-	void lowerTemperature();
-	int getRandomInt();
-	double getRandomDistribution();
-	double getPossibility(std::vector<std::pair<int, int>> newInstance);
+		void lowerTemperature();
+		int getRandomInt();
+		double getRandomDistribution();
+		double getPossibility(std::vector<std::pair<int, int>> newInstance);
 
-	virtual std::vector<std::pair<int, int>> & generateMovement();
-	bool isBetterResult();
+		virtual std::vector<std::pair<int, int>> & generateMovement();
+		bool isBetterResult();
 };
