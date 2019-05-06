5a9453e8f68da009ffcca9692eb24097dadb4d81
Turandor <48029803+Turandor@users.noreply.github.com>
2019-05-06 12:47:25 +0200

    przerwania

--- Lab1/Schrage.cpp
+++ Lab1/Schrage.cpp
@@ -50,6 +50,76 @@ void Schrage::operator() ()
 
 }
 
+void Schrage::SchragePmtn() 
+{
+	unsigned int time{}; /*min r*/
+	unsigned int i{}; /*number */
+	unsigned int Cmax{ 0 };
+
+	std::vector<RPQ> order;
+	
+	std::vector<RPQ> listaprzerwan;
+
+	while (!orderedTask.empty() || !notOrderedTask.empty())
+	{
+		while (!notOrderedTask.empty() && notOrderedTask.top().R <= time)
+		{
+			orderedTask.push(notOrderedTask.top());
+			notOrderedTask.pop();
+
+		}
+
+		if (orderedTask.empty())
+		{
+			time = notOrderedTask.top().R;
+		}
+		else
+		{
+
+			while (!listaprzerwan.empty())
+				listaprzerwan.pop_back();
+
+			for (int i = 0; i < (int)notOrderedTask.size(); i++)
+				if ((orderedTask.top().P + time > notOrderedTask.top().R) && (orderedTask.top().Q < notOrderedTask.top().Q))
+					listaprzerwan.push_back(notOrderedTask.top());
+
+			if (listaprzerwan.empty())
+			{
+				order.push_back(orderedTask.top());
+				orderedTask.pop();
+				time += order.back().P;
+
+				if (time + order.back().Q > Cmax)
+					Cmax = time + order.back().Q;
+			}
+
+			else 
+			{
+				RPQ part1 = orderedTask.top();
+				orderedTask.pop();
+				RPQ part2 = part1;
+
+				part1.P = listaprzerwan.back().R - time;
+				part2.P -= part1.P;
+				orderedTask.push(part2);
+
+				order.push_back(part1);
+				
+				time += order.back().P;
+
+				if (time + order.back().Q > Cmax)
+					Cmax = time + order.back().Q;
+
+
+
+			}
+
+		}
+
+	}
+	std::cout << Cmax << std::endl;
+
+}
 
 Schrage::~Schrage()
 {
