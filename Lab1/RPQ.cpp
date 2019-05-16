323a90614a304eb1a913a2ff8982c7324d04feec
Turandor <48029803+Turandor@users.noreply.github.com>
2019-05-16 23:58:39 +0200

    coś nie działa

---
+++ Lab1/RPQ.cpp
@@ -0,0 +1,11 @@
+#include "RPQ.h"
+
+
+
+RPQ::RPQ(unsigned int r, unsigned int p, unsigned int q, unsigned int nr) : R{ r }, P{ p }, Q{ q }, NR{ nr }
+{}
+
+
+RPQ::~RPQ()
+{
+}
--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,104 +1,64 @@
-#include "JohnsonsAlgorithm.h"
-#include "NEH.h"
-#include "main.h"
-#include "SA_algorithm.h"
-void testForJohnsonsAlgorithm();
+#include"Schrage.h"
+#include"Carlier.h"
+#include <iostream>
+#include <cstdio>
+#include <ctime>
+
+void TestSchrageOnAllMachines();
+void TestCarlierOnAllMachines();
 
 int main()
 {
-	int a;
-	//testForJohnsonsAlgorithm();
-
-	//Test1Neh();
-	//Test2Neh();
-	//Test3Neh();
-	std::vector<int> results;
-	double sum = 0;
-
-	for (int i = 0; i < 10; i++)
-	{
-		SA example(100, 0.1);
-		example.setExampleFromFile("cos.txt");
-		example.StartComputing();
-		results.push_back(example._endTime);
-		sum += example._endTime;
-	}
-	std::cout << std::endl;
-	for (auto e : results)
-	{
-		std::cout << " " << e;
-	}
-	sum = sum / 10;
-	std::cout << std::endl << sum << std::endl;
-
-	std::cin >> a;
+	//clock_t start = clock();
+	
+	int pause;
+	//TestSchrageOnAllMachines();
+	TestCarlierOnAllMachines();
+	//printf("Czas wykonywania: %lu ms\n", clock() - start);
+	std::cin >> pause;
 	return 0;
 }
 
-void Test1Neh()
+void TestSchrageOnFile(std::string fileName)
 {
-	NEH algorithm;
-	Machine first, second, third;
-	first.LoadTimes({ { 1,12 },{ 2,9 },{ 3,6 },{ 4,3 } });
-	second.LoadTimes({ { 1,11 },{ 2,8 },{ 3,5 },{ 4,2 } });
-	third.LoadTimes({ { 1,10 },{ 2,7 },{ 3,4 },{ 4,1 } });
-
-	algorithm.LoadData(first);
-	algorithm.LoadData(second);
-	algorithm.LoadData(third);
-
-	algorithm.DO();
-	std::cout << "Optimal result : " << std::endl;
+	Schrage tmp1;
+	SchragePmtn tmp2;
+	tmp1.LoadTasks(fileName);
+	tmp1();
+	//tmp2();
 }
 
-void Test2Neh()
+void TesCarlierOnFile(std::string fileName)
 {
-	NEH algorithm;
-	Machine first, second, third;
-
-	first.LoadTimes({ { 1,4 }, { 2,4 }, { 3,1 }, { 4,5 } });
-	second.LoadTimes({ {1,1},{2,3}, {3,2}, {4,1} });
-	third.LoadTimes({ {1,4},{2,3},{3,3},{4,3} });
-
-	algorithm.LoadData(first);
-	algorithm.LoadData(second);
-	algorithm.LoadData(third);
-
-	algorithm.DO();
-
-	std::cout << "Optimal result : " << std::endl;
+	Carlier tmp;
+	tmp.schrage.LoadTasks(fileName);
+	tmp.schragePmtn.LoadTasks(fileName);
+	tmp.carlier();
+	//tmp2();
 }
 
-void Test3Neh()
+void TestSchrageOnAllMachines()
 {
-	NEH algorithm;
-	Machine first, second, third;
-	/*
-	first.LoadTimes({ {1,1}, {2,9},{3,7},{4,4} });
-	second.LoadTimes({ {1,3},{2,3},{3,8},{4,8} });
-	third.LoadTimes({ {1,8},{2,5},{3,6},{4,7} });
-	algorithm.LoadData(first);
-	algorithm.LoadData(second);
-	algorithm.LoadData(third);
-	*/
-	algorithm.LoadFromFile("cos.txt");
-	algorithm.DO();
-
-	std::cout << "Optimal result : " << std::endl;
+	std::vector<std::string> a{ "in50.txt", "in100.txt", "in200.txt" };
+	for (auto e : a)
+	{
+		clock_t start = clock();
+		printf("%s\n	", e.c_str());
+		//std::cout << e << "  ";
+		TestSchrageOnFile(e);
+		printf("Czas wykonywania: %lu ms\n", clock() - start);
+	}
 }
 
-void testForJohnsonsAlgorithm()
+void TestCarlierOnAllMachines()
 {
-	int time;
-	Algorithm exampleAlgorithm;
-	timesForJobs exampleTimes{ { 1,1 },{ 2,2 },{ 3,3 },{ 4,4 },{ 5,5 } };
-	exampleAlgorithm.LoadTimesForMachines(Algorithm::MachineNumber::First, exampleTimes);
-	timesForJobs exampleTimes2{ { 1,2 },{ 2,3 },{ 3,4 },{ 4,5 },{ 5,6 } };
-	exampleAlgorithm.LoadTimesForMachines(Algorithm::MachineNumber::Second, exampleTimes2);
-	timesForJobs exampleTimes3{ { 1,3 },{ 2,2 },{ 3,1 },{ 4,4 },{ 5,5 } };
-	exampleAlgorithm.LoadTimesForMachines(Algorithm::MachineNumber::Third, exampleTimes3);
-	time = exampleAlgorithm.countTime(exampleAlgorithm.JohnsonsThreeMachineAlgorithm(), true);
-	std::cout << "Czas wykonania zadan wedlog reguly Johnson'a wynosi dla 3 maszyn: " << time << std::endl;
-	time = exampleAlgorithm.permutations(true);
-	std::cout << "Czas wykonania zadan po sprawdzeniu wszystkich mozliwosci dla 3 maszyn wynosi: " << time << std::endl;
+	std::vector<std::string> a{ "in100.txt" };
+	for (auto e : a)
+	{
+		clock_t start = clock();
+		printf("%s\n	", e.c_str());
+		//std::cout << e << "  ";
+		TesCarlierOnFile(e);
+		printf("Czas wykonywania: %lu ms\n", clock() - start);
+	}
 }
